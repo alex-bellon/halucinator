41bb20fbc470bf356b02892aaa460eebcc8e5980
Alex Bellon <abellon@ucsd.edu>
2022-07-07 22:36:11 +0000

    Added custom install script, added dependency

---
+++ install.sh
@@ -0,0 +1,25 @@
+HALUCINATOR_ROOT="$HOME/halucinator"
+
+cd $HALUCINATOR_ROOT
+./install_deps.sh
+
+VIRT_ENV="halucinator"
+python3 -m venv ~/.virtualenvs/"$VIRT_ENV"
+
+export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3
+export WORKON_HOME=$HOME/.virtualenvs
+export VIRTUALENVWRAPPER_VIRTUALENV=$HOME/.local/bin/virtualenv
+source /usr/local/bin/virtualenvwrapper.sh
+
+pip install -r src/requirements.txt
+pip install -e src
+
+cd $HALUCINATOR_ROOT/deps/avatar2
+pip install -e .
+cd $HALUCINATOR_ROOT/deps/avatar2/targets
+./build_qemu.sh
+
+export HALUCINATOR_QEMU_ARM=$HALUCINATOR_ROOT/deps/avatar2/targets/build/qemu/arm-softmmu/qemu-system-arm
+export HALUCINATOR_QEMU_ARM64=$HALUCINATOR_ROOT/deps/avatar2/targets/build/qemu/aarch64-softmmu/qemu-system-aarch64
+
+sudo ln /usr/bin/gdb-multiarch /usr/bin/arm-none-eabi-gdb
--- install_deps.sh
+++ install_deps.sh
@@ -18,7 +18,8 @@ case $DISTRO in
   "debian" | "ubuntu" )
     sudo apt-get install -y ethtool python-tk gdb-multiarch tcpdump \
                             python3-pip python3-venv cmake g++ \
-                            build-essential libpixman-1-dev
+                            build-essential libpixman-1-dev \
+			    pkg-config
     ;;
   *) echo "Distro not supported" ;;
 esac
