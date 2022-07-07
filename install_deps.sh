8d4e5acd6cbaabaeb43e532fcd3675c7534882c1
alex-bellon <alexrbellon@gmail.com>
2022-07-07 12:25:24 -0700

    Added arch deps installation

--- install_deps.sh
+++ install_deps.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 # . /etc/bash_completion
 set -e 
-#set -x
-sudo apt-get install -y ethtool python-tk gdb-multiarch tcpdump python3-pip \
-                        python3-venv cmake g++ build-essential libpixman-1-dev
+set -x
+
+DISTRO=none
+
+if [ -f /etc/os-release ]; then
+  . /etc/os-release
+  DISTRO="${ID_LIKE:-$ID}"
+fi
+
+case $DISTRO in
+  "arch" )
+    sudo pacman -S ethtool tk gdb tcpdump python-pip \
+                   python-virtualenv cmake gcc base-devel pixman
+    ;;
+  "debian" | "ubuntu" )
+    sudo apt-get install -y ethtool python-tk gdb-multiarch tcpdump \
+                            python3-pip python3-venv cmake g++ \
+                            build-essential libpixman-1-dev
+    ;;
+  *) echo "Distro not supported" ;;
+esac
 
 sudo pip3 install virtualenv virtualenvwrapper
-
 
 # VIRT_ENV="halucinator"
 # python3 -m venv ~/.virtualenvs/"$VIRT_ENV"
